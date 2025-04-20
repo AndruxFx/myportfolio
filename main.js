a947f2e89c85e4bdc93602c71729cac456e8d38e
AndruxFx <andresfernandez.it@gmail.com>
2025-04-20 20:36:40 -0300

    js in home, first part

---
+++ main.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.typed', {
+    strings:[
+         '<i class= "tpdtext"> Coder </i>'
+        ,'<i class= "tpdtext"> Fullstack Developer </i>'
+        ,'<i class= "tpdtext"> Web Designer </i>'
+    ],
+    typeSpeed: 75,
+    startDelay: 300,
+    backspeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay: 1500,
+    loop:true,
+    loopCount:false,
+    showCursor:true,
+    CursorChar:'|',
+    contentTyped:'html',
+});
